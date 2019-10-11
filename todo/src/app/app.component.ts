9d4588f25fc6a4dea905235748c680c0e8af8038
Fernando Longuini <fernando.silva236@fatec.sp.gov.br>
2019-10-11 14:30:51 -0300

    Aula16: Adicionando itens ao todo

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -20,10 +20,17 @@ export class AppComponent {
 			Validators.required
 		])]
 	});
+  }
 
-	this.todos.push(new Todo(1,'Primeira tarefa',false));
-	this.todos.push(new Todo(2,'Segunda tarefa',true));
-	this.todos.push(new Todo(3,'Terceira tarefa',false));
+  add(){
+	  const title = this.form.controls['title'].value;
+	  const id = this.todos.length + 1;
+		this.todos.push(new Todo(id, title, false));
+		this.clear();
+  }
+
+  clear(){
+	  this.form.reset();
   }
 
   remove(todo: Todo){
